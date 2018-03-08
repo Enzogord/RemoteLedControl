9571873dc18a2a3f4578403d0256c59c3fe0ecf7
Enzo <Enzo@ENZO-NOTEBOOK>
2018-03-08 16:51:00 +0300

    Перенесены нумераторы в отдельный файл

--- Core/Core.cs
+++ Core/Core.cs
@@ -13,19 +13,6 @@ using System.Collections.ObjectModel;
 
 namespace Core
 {
-    public enum ProjectServerMode
-    {
-        Test = 1,
-        Release = 2
-    }
-
-    public enum ClientState
-    {
-        Wait = 1,
-        Play = 2,
-        Pause = 3
-    }
-
     /// <summary>
     /// Основной класс осуществляющий всю работу сервера, клиентов, вывода информации
     /// </summary>
---
+++ Core/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public enum ProjectServerMode
+    {
+        Test = 1,
+        Release = 2
+    }
+
+    public enum ClientState
+    {
+        Wait = 1,
+        Play = 2,
+        Pause = 3
+    }
+}
