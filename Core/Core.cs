4a5bd190415270d947a085f5971f7b295283f7a5
Enzo <Enzo@ENZO-NOTEBOOK>
2018-03-08 17:05:39 +0300

    Перенесен код XML saver'а в отдельный файл

--- Core/Core.cs
+++ Core/Core.cs
@@ -14,45 +14,6 @@ using System.Collections.ObjectModel;
 namespace Core
 {
     /// <summary>
-    /// Класс осуществляющий сохранение данных в XML файл
-    /// </summary>
-    public class XMLSaver
-    {
-        public Project Fields;
-
-        //Запись настроек в файл
-        public void WriteXml(string SavePath)
-        {
-            if (Fields != null)
-            {
-                XmlWriterSettings settings = new XmlWriterSettings() { Indent = true };
-                XmlWriter w = XmlDictionaryWriter.Create(SavePath, settings);
-                NetDataContractSerializer ser = new NetDataContractSerializer();
-                ser.WriteObject(w, Fields);
-                w.Close();
-            }
-
-
-        }
-        //Чтение насроек из файла
-        public void ReadXml(string OpenPath)
-        {
-            if (Fields != null)
-            {
-                if (File.Exists(OpenPath))
-                {
-                    FileStream fs = new FileStream(OpenPath, FileMode.Open);
-                    XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
-                    NetDataContractSerializer ser = new NetDataContractSerializer();
-                    Fields = (ser.ReadObject(reader, true) as Project);
-                    fs.Close();
-
-                }
-            }
-        }
-    }
-
-    /// <summary>
     /// Класс осуществляющий передачу и прием информации с клиентов по протоколу UDP
     /// </summary>
     [DataContract]
