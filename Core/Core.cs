3eecb2c4c4812d7704ed6e4de1efef79f913f1f9
Enzo <Enzo@ENZO-NOTEBOOK>
2018-03-08 17:01:32 +0300

    Перенесен код для пина в отдельный файл

--- Core/Core.cs
+++ Core/Core.cs
@@ -14,61 +14,6 @@ using System.Collections.ObjectModel;
 namespace Core
 {
     /// <summary>
-    ///  Информация о пине используемом на плате клиента
-    /// </summary>
-    [DataContract]
-    public class Pin
-    {
-        // Fields
-        [DataMember]
-        private byte PPinNumber;
-        [DataMember]
-        private ushort PLEDCount;
-
-        // Propeties
-        public byte PinNumber
-        {
-            get { return PPinNumber; }
-            set
-            {
-                PPinNumber = value;
-                OnChange?.Invoke();
-            }
-        }
-        public ushort LEDCount
-        {
-            get { return PLEDCount; }
-            set
-            {
-                PLEDCount = value;
-                OnChange?.Invoke();
-            }
-        }
-
-        // Events
-        public delegate void Change();
-        public event Change OnChange;
-
-        // Methods
-        public Pin(string _Pin, string _LEDCount)
-        {
-            byte TmpPin;
-            if (!byte.TryParse(_Pin, out TmpPin))
-            {
-                return;
-            }
-            ushort TmpLEDCount;
-            if (!ushort.TryParse(_LEDCount, out TmpLEDCount))
-            {
-                return;
-            }
-            PinNumber = TmpPin;
-            LEDCount = TmpLEDCount;
-        }
-
-    }
-
-    /// <summary>
     /// Информация о циклограмме воспроизводимой клиентом
     /// </summary>
     [DataContract]
