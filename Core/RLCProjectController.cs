f3d296ad0135c78afabfc07a736d79727efbe0d2
Enzo <Enzo@ENZO-NOTEBOOK>
2019-02-02 20:46:42 +0300

    Добавлен контроллер проектов, осуществляющий запуск, загрузку, сохранение проектов

---
+++ Core/RLCProjectController.cs
@@ -0,0 +1,37 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class RLCProjectController : NotifyPropertyBase
+    {
+        private Project currentProject;
+
+        public Project CurrentProject {
+            get => currentProject;
+            set {
+                currentProject = value;
+                SetField(ref currentProject, value, () => CurrentProject);
+            }
+        }
+
+        public RLCProjectController()
+        {
+
+        }
+
+        public void LoadProject()
+        {
+
+        }
+
+        public void SaveProject()
+        {
+
+        }
+    }
+}
