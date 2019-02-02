505e9aa4d667b6026808bcbcdd65dc94c0c3f422
Enzo <Enzo@ENZO-NOTEBOOK>
2019-02-02 20:19:24 +0300

    Добавлен проект с сервисными классами

---
+++ Service/NotifyPropertyBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteLEDControl.Service
+{
+    public abstract class NotifyPropertyBase : INotifyPropertyChanged
+    {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged<T>(Expression<Func<T>> selectorExpression)
+        {
+            if (selectorExpression == null) {
+                throw new ArgumentNullException("selectorExpression");
+            }
+            MemberExpression body = selectorExpression.Body as MemberExpression;
+            if (body == null) {
+                throw new ArgumentException("The body must be a member expression");
+            }
+            OnPropertyChanged(body.Member.Name);
+        }
+
+        protected bool SetField<T>(ref T field, T value, Expression<Func<T>> selectorExpression)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(selectorExpression);
+            return true;
+        }
+    }
+}
---
+++ Service/Service.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+
+namespace RemoteLEDControl.Service
+{
+    public static class Service
+    {
+        /// <summary>
+        /// Находит к какой подсети принадлежит адрес с указанной маской подсети
+        /// </summary>
+        /// <param name="address">IP адрес по которому необходимо найти адрес подсети</param>
+        /// <param name="subnetMask">Маска подсети указанного в первом параметре адреса</param>
+        /// <returns>Адрес подсети</returns>
+        /// <exception cref="ArgumentException"></exception>
+        private static IPAddress GetNetworkAddress(IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+
+            byte[] networkAddress = new byte[ipAdressBytes.Length];
+
+            for (int i = 0; i < networkAddress.Length; i++)
+            {
+                networkAddress[i] = (byte)(ipAdressBytes[i] & (subnetMaskBytes[i]));
+            }
+
+            return new IPAddress(networkAddress);
+        }
+
+        /// <summary>
+        /// Находит широковещательный адрес подсети к которой принадлежит адрес с указанной маской подсети
+        /// </summary>
+        /// <param name="address">IP адрес по которому необходимо найти широковещательный адрес подсети</param>
+        /// <param name="subnetMask">Маска подсети указанного в первом параметре адреса</param>
+        /// <returns>Широковещательынй адрес подсети</returns>
+        /// <exception cref="ArgumentException"></exception>
+        private static IPAddress GetBroadcastAddress(IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+
+            byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] | (subnetMaskBytes[i] ^ 255));
+            }
+
+            return new IPAddress(broadcastAddress);
+        }
+    }
+}
