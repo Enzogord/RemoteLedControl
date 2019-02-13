e26050a712bc9e78f147f3cd462b383754518796
Enzo <Enzo@ENZO-NOTEBOOK>
2019-02-13 22:21:44 +0300

    Pin переделан под NotifyPropertyBase

--- Core/Pin.cs
+++ Core/Pin.cs
@@ -1,15 +1,25 @@
-﻿using System.Runtime.Serialization;
+﻿using Service;
+using System.Runtime.Serialization;
 
 namespace Core
 {
     [DataContract]
-    public class Pin
+    public class Pin : NotifyPropertyBase
     {
+        private byte pinNumber;
         [DataMember]
-        public byte PinNumber { get; set; }
+        public byte PinNumber {
+            get => pinNumber;
+            set => SetField(ref pinNumber, value, () => PinNumber);
+        }
 
+        private int ledCount;
         [DataMember]
-        public ushort LEDCount { get; set; }
+        public int LEDCount {
+            get => ledCount;
+            set => SetField(ref ledCount, value, () => LEDCount);
+        }
+
 
         public Pin(byte pin, ushort ledCount)
         {
